332b39c50fbf0983bc70979b88d1803d60785d45
ViskaWei <swei20@jhu.edu>
2022-03-01 12:57:20 -0500

    init

--- PIML/gateway/dataIF/basedataIF.py
+++ PIML/gateway/dataIF/basedataIF.py
@@ -13,33 +13,47 @@ class BaseDataIF(ABC):
         pass
 
     @abstractmethod
-    def prepare_data(self, ):
+    def process_data(self, ):
         pass
 
-
-
+    @abstractmethod
+    def prepare(self, param, data):
+        pass
 
 class WaveDataIF(BaseDataIF):
     """ class for separate data into detector arms. """
+    def __init__(self) -> None:
+        super().__init__()
+        self.startIdx = None
+        self.endIdx = None
+
     def set_param(self, param):
         self.wRng = (param.wStart, param.wEnd)
 
     def set_data(self, data):
         self.wave = data.wave
-        self.flux = data.flux
     
-    def prepare_data(self, ):
-        np.digitize(self.wRng, self.wave)
+    def process_data(self):
+        self.startIdx, self.endIdx = np.digitize(self.wRng, self.wave)
+    
+    def prepare(self, param, data):
+        self.set_param(param)
+        self.set_data(data)
+        self.process_data()
 
-
-
-    def load_data(self, loader, args):
-        pass
 
 class BoxDataIF(BaseDataIF):
     """ class for loading data into box. """
     def __init__(self) -> None:
         super().__init__()
 
-    def set_params(self, boxParams):
-        self.R = boxParams["R"]
+    def set_param(self, param):
+        self.R = param.R
+
+    def set_data(self, data):
+        self.para = data.para
+
+    def process_data(self):
+        dfpara = pd.DataFrame(para, columns=["M","T","G","C","O"])
+    
+    def prepare(self, param, data):
