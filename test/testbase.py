f07996ed4f46e7248de6026e24e3fd042343f160
ViskaWei <swei20@jhu.edu>
2022-03-01 23:43:25 -0500

    updates

--- test/testbase.py
+++ test/testbase.py
@@ -1,11 +1,14 @@
 from unittest import TestCase
 
 class TestBase(TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+
+        self.DATA_PATH = "test/testdata/bosz_5000_test.h5"
+
+        self.params = {"DATA_PATH": self.DATA_PATH
+                        "wStart": 71.0,
+        }
 
 
-
-
-
-    # def test_base(self):
-    #     self.assertTrue(True)
-    pass
+    
